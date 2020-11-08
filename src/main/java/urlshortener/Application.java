436845e2af0572ac69eba5cc516ff186a93bcccb
Alberto Calvo <albertocalvojaca@gmail.com>
2020-11-08 11:11:45 +0100

    GSB_API_KEY Env var

--- src/main/java/urlshortener/Application.java
+++ src/main/java/urlshortener/Application.java
@@ -9,11 +9,19 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class Application extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
+    checkEnvVars();
     SpringApplication.run(Application.class, args);
   }
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     return application.sources(Application.class);
+  }
+
+  private static void checkEnvVars() {
+    String API_KEY = System.getenv("GSB_API_KEY");
+    if (API_KEY == null) {
+      throw new RuntimeException("GSB_API_KEY not established as env variable");
+    }
   }
 }
