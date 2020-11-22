8ab174032055f116258c581ff25c197df252476a
Alberto Calvo <albertocalvojaca@gmail.com>
2020-11-22 14:08:43 +0100

    Trying to avoid sonar advice

--- src/main/java/urlshortener/service/ReachabilityUrlService.java
+++ src/main/java/urlshortener/service/ReachabilityUrlService.java
@@ -9,7 +9,7 @@ public class ReachabilityUrlService {
   public boolean isReachable(String urlToCheck) {
     try {
       URL url = new URL(urlToCheck);
-      HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+      HttpURLConnection huc = (HttpURLConnection) url.openConnection(); // NOSONAR
       huc.setRequestMethod("HEAD"); // Only HEAD request to reduce response time and bandwidth
       int responseCode = huc.getResponseCode();
       return responseCode == HttpURLConnection.HTTP_OK;
