08352671be9a21f8c78372bfe06be6ba4e98a659
jgarciapueyo <jgarciapueyo@gmail.com>
2020-12-29 20:34:22 +0100

    Migrate Click Repository

--- src/main/java/urlshortener/domain/Click.java
+++ src/main/java/urlshortener/domain/Click.java
@@ -1,66 +1,27 @@
 package urlshortener.domain;
 
 import java.sql.Date;
+import javax.persistence.*;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
 public class Click {
 
-  private final Long id;
-  private final String hash;
-  private final Date created;
-  private final String referrer;
-  private final String browser;
-  private final String platform;
-  private final String ip;
-  private final String country;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-  public Click(
-      Long id,
-      String hash,
-      Date created,
-      String referrer,
-      String browser,
-      String platform,
-      String ip,
-      String country) {
-    this.id = id;
-    this.hash = hash;
-    this.created = created;
-    this.referrer = referrer;
-    this.browser = browser;
-    this.platform = platform;
-    this.ip = ip;
-    this.country = country;
-  }
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "hash", nullable = false)
+  private ShortURL hash;
 
-  public Long getId() {
-    return id;
-  }
-
-  public String getHash() {
-    return hash;
-  }
-
-  public Date getCreated() {
-    return created;
-  }
-
-  public String getReferrer() {
-    return referrer;
-  }
-
-  public String getBrowser() {
-    return browser;
-  }
-
-  public String getPlatform() {
-    return platform;
-  }
-
-  public String getIp() {
-    return ip;
-  }
-
-  public String getCountry() {
-    return country;
-  }
+  private Date created;
+  private String referrer;
+  private String browser;
+  private String platform;
+  private String ip;
+  private String country;
 }
