b5b4fa15e4214d2909e8ab3c510faea659a2af4d
Luis Garcia <luisgarcia98@hotmail.es>
2020-12-30 21:05:53 +0100

    change format

--- src/main/java/urlshortener/service/exceptions/BadCustomBackhalfException.java
+++
@@ -1,8 +0,0 @@
-package urlshortener.service.exceptions;
-
-public class BadCustomBackhalfException extends Exception {
-
-  public BadCustomBackhalfException(String errorMessage) {
-    super(errorMessage);
-  }
-}
