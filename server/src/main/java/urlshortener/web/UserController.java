60cd726bb089b22505b7260b6c8ac545695ef53b
jgarciapueyo <jgarciapueyo@gmail.com>
2021-01-10 01:19:38 +0100

    Add Keycloak connection and registration in /user

---
+++ server/src/main/java/urlshortener/web/UserController.java
@@ -0,0 +1,20 @@
+package urlshortener.web;
+
+import org.keycloak.KeycloakSecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserController {
+
+    @GetMapping(value = "/user")
+    public String customers(Principal principal) {
+
+        return "user " + principal.getName();
+    }
+}
