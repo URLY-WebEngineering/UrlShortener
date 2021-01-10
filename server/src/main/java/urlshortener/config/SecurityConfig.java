1465d8f699940a6ad36b214c46abb516aee8ae03
Alberto Calvo <albertocalvojaca@gmail.com>
2021-01-10 20:22:29 +0100

    Google Java Format

--- server/src/main/java/urlshortener/config/SecurityConfig.java
+++ server/src/main/java/urlshortener/config/SecurityConfig.java
@@ -21,37 +21,35 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
-    @Autowired
-    public void configureGlobal(
-            AuthenticationManagerBuilder auth) throws Exception {
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider
-                = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
-                new SimpleAuthorityMapper());
-        auth.authenticationProvider(keycloakAuthenticationProvider);
-    }
+    KeycloakAuthenticationProvider keycloakAuthenticationProvider =
+        keycloakAuthenticationProvider();
+    keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+    auth.authenticationProvider(keycloakAuthenticationProvider);
+  }
 
-    @Bean
-    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
-    }
+  @Bean
+  public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+    return new KeycloakSpringBootConfigResolver();
+  }
 
-    @Bean
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(
-                new SessionRegistryImpl());
-    }
+  @Bean
+  @Override
+  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/user/**")
-                .hasRole("user")
-                .anyRequest()
-                .permitAll();
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    super.configure(http);
+    http.csrf()
+        .disable()
+        .authorizeRequests()
+        .antMatchers("/user/**")
+        .hasRole("user")
+        .anyRequest()
+        .permitAll();
+  }
 }
--- server/src/main/java/urlshortener/web/UserController.java
+++ server/src/main/java/urlshortener/web/UserController.java
@@ -1,20 +1,15 @@
 package urlshortener.web;
 
-import org.keycloak.KeycloakSecurityContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import java.security.Principal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 public class UserController {
 
-    @GetMapping(value = "/user")
-    public String customers(Principal principal) {
+  @GetMapping(value = "/user")
+  public String customers(Principal principal) {
 
-        return "user " + principal.getName();
-    }
+    return "user " + principal.getName();
+  }
 }
