bcaaa2661bb617d1a5482216ab1711a408354f8e
jgarciapueyo <jgarciapueyo@gmail.com>
2021-01-11 23:24:48 +0100

    Add login/logout in frontend

--- server/src/main/resources/static/js/app.js
+++ server/src/main/resources/static/js/app.js
@@ -1,40 +1,35 @@
+function successShortLink(msg) {
+    $("#result").html(
+        `<div class="alert alert-success lead">
+            <a target="_blank" href="${msg.uri}"> ${msg.uri} </a>
+        </div>`);
+
+    if (msg.qr != null) {
+        $("#QRresult").html(
+            `<img style="width:400px;height:400px;" src="${msg.qr}" />`
+        );
+    }
+}
+
+function errorShortLink(msg) {
+    $("#result").html("<div class='alert alert-danger lead'>SOMETHING WENT WRONG</div>");
+}
+
 $(document).ready(
     function () {
-        $("#shortener").submit(
-            function (event) {
-                event.preventDefault();
-                $.ajax({
-                    type: "POST",
-                    url: "/link",
-                    data: $(this).serialize(),
-                    success: function (msg) {
-                        $("#result").html(
-                            "<div class='alert alert-success lead'><a target='_blank' href='"
-                            + msg.uri
-                            + "'>"
-                            + msg.uri
-                            + "</a></div>"
-                            );
-                        if (msg.qr != null){
-                            $("#QRresult").html(
-                                "<img  style='width:400px;height:400px;'"
-                                + "src='"
-                                + msg.qr
-                                +"' />"
-                            );
-                        }
-                        else{
-                            $("#QRresult").html(
-                                '<div id="QRresult" >' +   ' </div>'
-                            );
-                        }
-
-                    },
-                    error: function () {
-                        // TODO: think about displaying the error from server
-                        $("#result").html(
-                            "<div class='alert alert-danger lead'>SOMETHING WENT WRONG</div>");
-                    }
-                });
+        $("#shortener").submit(function (event) {
+            event.preventDefault();
+            $.ajax({
+                type: "POST",
+                url: "/link",
+                data: $(this).serialize(),
+                success: function (msg) {
+                    successShortLink(msg)
+                },
+                error: function (msg) {
+                    errorShortLink(msg)
+                }
             });
-    });
+        });
+    }
+);
